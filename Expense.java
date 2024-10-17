acec5c49f1d7861177d2eec738f245b304dc5646
Jmends <jonathanamends7@gmail.com>
2024-10-16 22:41:33 -0500

    created class and object fields for expenses

---
+++ Expense.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+
+public class Expense {
+    private int id;
+    private LocalDate date;
+    private String description;
+    private double amount;
+
+    public Expense(int id, String description, double amount) {
+        this.id = id;
+        this.date = LocalDate.now();
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String toString() {
+        return id + "\t" + date + "\t" + description + "\t" + amount;
+    }
+
+}
