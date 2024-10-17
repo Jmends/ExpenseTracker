5aece1424b3edbd71ab42253bfec55f2840eedf3
Jmends <jonathanamends7@gmail.com>
2024-10-16 22:42:27 -0500

    Started class that handles expense operations

--- ExpenseManager.java
+++ ExpenseManager.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpenseManager {
-    
+    private List<Expense> expenses;
+    private static final String FILE_NAME = "Expenses.txt";
+
+    public ExpenseManager() {
+        expenses = new ArrayList<>();
+    }
+
+    public void addExpense(double amount, String description) {
+
+    }
+
+    public void deleteExpense(int id) {
+
+    }
+
+    public void editExpense(int id, String newDescription, double newAmount) {
+
+    }
+
+    public void listExpense() {
+
+    }
 }
