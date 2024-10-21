c3079a80ff3421f3385bc3f6784596ef4eeaa217
Jmends <jonathanamends7@gmail.com>
2024-10-21 18:08:50 -0500

    started the main program. Added switch statments and an if statement to handle no arguments being passed by user

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,5 +1,29 @@
+
 public class ExpenseTracker {
     public static void main(String[] args) {
+        ExpenseManager manager = new ExpenseManager();
+        
 
+        if(args.length == 0){
+            System.out.println("Please provide a command");
+            return;
+        }
+
+        String command = args[0];
+
+        switch (command) {
+            case "add":
+                break;
+            case "delete":
+                break;
+            case "edit":
+                break;
+            case "list":
+                break;
+            case "summary":
+                break;
+            default:
+                break;
+        }
     }
 }
