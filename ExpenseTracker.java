8a7e02d9acfb2b23fecefe898b981dd255fcb8cf
Jmends <jonathanamends7@gmail.com>
2024-10-22 22:08:37 -0500

    added the main logic fo radding and deleting expenses

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -2,19 +2,30 @@
 public class ExpenseTracker {
     public static void main(String[] args) {
         ExpenseManager manager = new ExpenseManager();
-        
 
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Please provide a command");
             return;
         }
 
         String command = args[0];
+        
 
         switch (command) {
             case "add":
+                if (args.length == 5 && args[1].equals("--description")
+                        && args[3].equals("--amount")) {
+                    String description = args[2];
+                    double amount = Double.parseDouble(args[4]);
+                    manager.addExpense(amount, description);
+
+                }
                 break;
             case "delete":
+                if(args.length == 3){
+                    int id = Integer.parseInt(args[2]);
+                    manager.deleteExpense(id);
+                }
                 break;
             case "edit":
                 break;
