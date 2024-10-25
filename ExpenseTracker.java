facf55da407ea4dfdc5e1ae3d24977d3b1b38fea
Jmends <jonathanamends7@gmail.com>
2024-10-24 22:01:18 -0500

    added the case for editing, listing and the summary

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,7 +1,8 @@
-
+import java.util.Scanner;
 public class ExpenseTracker {
     public static void main(String[] args) {
         ExpenseManager manager = new ExpenseManager();
+        Scanner scan = new Scanner(System.in);
 
         if (args.length == 0) {
             System.out.println("Please provide a command");
@@ -28,10 +29,33 @@ public class ExpenseTracker {
                 }
                 break;
             case "edit":
+                if(args.length == 1){
+                    manager.listExpense();
+                    System.out.println("What is the ID of the expense youd like to edit? ");
+                    int newID = scan.nextInt();
+
+                    scan.nextLine();
+
+                    System.out.println("What is the new description? ");
+                    String newDescription = scan.nextLine();
+
+                    System.out.println("What is the new amount? ");
+                    double newAmount = scan.nextDouble();
+
+                    manager.editExpense(newID, newDescription, newAmount);
+
+
+                }
                 break;
             case "list":
+                if(args.length == 1){
+                    manager.listExpense();
+                }
                 break;
             case "summary":
+                if(args.length == 1){
+                    manager.summary();
+                }
                 break;
             default:
                 break;
